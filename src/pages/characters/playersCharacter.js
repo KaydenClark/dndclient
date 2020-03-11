99c1994d822e78e15b76ad33a8dca18a3ec17bf3
Kayden Clark <kaydenclark725@gmail.com>
2020-03-11 10:37:50 -0600

    got character names to load

---
+++ src/pages/characters/playersCharacter.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import axios from 'axios'
+import {
+    base
+} from '../../components/const'
+
+const API = base
+
+export default class PlayersCharacter extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            data: []
+        }
+    }
+
+    render(){
+
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
